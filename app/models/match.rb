b4d6417b317a9ff05e9aab92d8868955b5deefe7
Julianne Joswiak <juliannej@mac.com>
2016-11-22 11:24:24 +0000

    added a connection between wins and sport

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,8 @@
 class Match < ApplicationRecord
   belongs_to :competition
+  has_one :sport, through: :competition
   belongs_to :winner, :class_name => 'User', :foreign_key => 'winner_id'
+
 
   has_many :match_participants
   has_many :players, through: :match_participants
