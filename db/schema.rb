75f1c9e95b7e0894ef5f70b8a9acc7d3f507a6a2
Julianne Joswiak <juliannej@mac.com>
2016-11-22 13:29:30 +0000

    added new methods for user to get various aspects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121181108) do
+ActiveRecord::Schema.define(version: 20161122121146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161121181108) do
     t.integer  "sport_id"
     t.integer  "creator_id"
     t.integer  "champion_id"
+    t.string   "status"
     t.index ["champion_id"], name: "index_competitions_on_champion_id", using: :btree
     t.index ["creator_id"], name: "index_competitions_on_creator_id", using: :btree
     t.index ["sport_id"], name: "index_competitions_on_sport_id", using: :btree
