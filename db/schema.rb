f850f811172987b5a9c54fd5086cf18af0a56a24
Julianne Joswiak <juliannej@mac.com>
2016-11-24 12:30:08 +0000

    seeding users and friendships

--- db/schema.rb
+++ db/schema.rb
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 20161123155110) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "city"
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                  default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
