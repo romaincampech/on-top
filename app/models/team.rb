b918c4506e83a76c81f49f81168d35551e7266e1
Julianne Joswiak <juliannej@mac.com>
2016-11-29 10:00:58 +0000

    dependent destroy added on team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,9 @@ class Team < ApplicationRecord
   has_attachment :photo
   belongs_to :captain, :class_name => 'User', :foreign_key => 'captain_id'
   belongs_to :sport, optional: true
-  has_many :team_memberships
+  has_many :team_memberships, dependent: :destroy
   has_many :users, through: :team_memberships
+  has_one :chat_room, dependent: :destroy
   validates_associated :captain
   validates_presence_of :captain
 
