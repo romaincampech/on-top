45826718259891fc7c1e343129728c10fa1d0366
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-01-31 00:04:45 +0000

    built match and fixture list components for trialing rendering react in  view

---
+++ app/assets/javascripts/components/fixture_list.js.jsx
@@ -0,0 +1,11 @@
+var FixtureList = React.createClass ({
+  render: function() {
+    return (
+      <div>
+        {this.props.matches.map(function(match){
+          return <Match match={match} key={match.id} />;
+        })}
+      </div>
+      );
+  }
+});
--- app/assets/javascripts/components/match.js.jsx
+++ app/assets/javascripts/components/match.js.jsx
@@ -1,36 +1,18 @@
 var Match = React.createClass({
   render: function(){
     return (
-      // <% if match.players.first == player || match.players.last == player %>
       <div className="match-league" id={this.props.match.match_number}>
-      // <% if match.players.first %>
-        // <%= link_to user_path(match.players.first), "data-toggle" => "tooltip", title: "#{match.players.first.full_name}" do %>
-        <div className="avatar-div">
-          <img src={this.props.match.player_one.photo}>
+        <div>
+          {this.props.match.player_one.first_name}
         </div>
-        // <% end %>
-        // <% end %>
         <span>vs</span>
-        // <% if match.players.last != match.players.first %>
-        // <%= link_to user_path(match.players.last), "data-toggle" => "tooltip", title: "#{match.players.last.full_name}" do %>
-        <div className="avatar-div">
-          <img src={this.props.match.player_two.photo}>
+        <div>
+          {this.props.match.player_two.first_name}
         </div>
-        // <% end %>
-        // <% end %>
-        // <% if current_user == @competition.creator %>
-        <div className="btn btn-sm league-result-input-btn", id={this.props.match.match_number}>
+        <div className="btn btn-sm league-result-input-btn" id={this.props.match.match_number}>
           Enter Score
         </div>
-        // <% elsif current_user == match.players.first || current_user == match.players.last %>
-        //   <div class="btn btn-sm league-result-input-btn", id=<%= "#{match.match_number}" %>>Enter Score</div>
-        // <% else %>
-        //   <p><em><%= match.status %></em></p>
-        // <% end %>
       </div>
-      <div className="score-form" id="edit-<%="#{match.match_number}" %>">
-        <%= render 'matches/edit_two', match: match %>
-      </div>
-
-      )
-  }})
+    );
+  }
+});
