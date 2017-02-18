b24bd8e01d82b2fc436d63f70fc42431fa7d7bb7
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-02-18 09:41:29 +0000

    displaying league  fixtures in react by player in progress

--- app/assets/javascripts/components/fixture_list.js.jsx
+++ app/assets/javascripts/components/fixture_list.js.jsx
@@ -6,22 +6,28 @@ var FixtureList = React.createClass ({
   },
 
   render: function() {
-    var players =
+    var players = this.props.matches[0]["competition_participants"];
+
+
+    for (i=0; i < players.length; i++) {
+      var arr = [];
+      var player_matches = [];
+      <div id={players[i].first_name}>
+        {this.props.matches.map(function(match){
+          if (match.player_one.id === match.competition_participants[i].id || match.player_two.id === match.competition_participants[i].id) {
+            arr.push(<Match match={match} key={match.id} score_params={match.score_params} />);
+          }
+        })}
+      </div>
+    }
+
+    console.log(players[0].first_name);
+    console.log(arr)
     return (
       <div>
-        for(i = 0; i < players.length; i++ ) {
-            <div id={players[i].first_name}>
-        {this.props.matches.map(function(match){
 
-            if (match.player_one.id === match.competition_participants[i].id || match.player_two.id ===match.competition_participants[i].id) {
-            return <Match match={match} key={match.id} score_params={match.score_params} />;
-            }
+       {arr}
 
-          }
-
-        )}
-             </div>
-      }
       </div>
     );
   }
