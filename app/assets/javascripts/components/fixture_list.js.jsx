ac22f17bbc4f01ac5b91e68ffb4e5224c4ba96ba
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-02-22 16:08:39 +0000

    league fixtures now diplay by player, and the tabs are working

--- app/assets/javascripts/components/fixture_list.js.jsx
+++ app/assets/javascripts/components/fixture_list.js.jsx
@@ -6,28 +6,19 @@ var FixtureList = React.createClass ({
   },
 
   render: function() {
+    var that = this
     var players = this.props.matches[0]["competition_participants"];
+    var arr=[];
+    console.log(players);
+    players.forEach(function(player, i){
 
-    var arr = [];
-    for (i=0; i < players.length; i++) {
-      arr[{i}] = [];
-      var player_matches = [];
-      <div id={players[i].first_name} key={i}>
-        {this.props.matches.map(function(match, j){
-          if (match.player_one.id === match.competition_participants[i].id || match.player_two.id === match.competition_participants[i].id) {
-            arr[{i}].push(<Match match={match} key={j} score_params={match.score_params} />);
-          }
-        })}
-      </div>
-    }
+        arr.push(<PlayerFixtures fixtures={that.props.matches} player={player} key={i} />);
 
-    console.log(players[0].first_name);
+    })
     console.log(arr)
     return (
       <div>
-
-       {arr}
-
+        {arr}
       </div>
     );
   }
