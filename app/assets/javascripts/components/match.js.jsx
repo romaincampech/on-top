55a3ba66bf81b2fdbe13052d3d66d9c1fbd71b28
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-02-26 00:43:23 +0000

    data returned from ajax now passed to parent component

--- app/assets/javascripts/components/match.js.jsx
+++ app/assets/javascripts/components/match.js.jsx
@@ -1,7 +1,6 @@
 var Match = React.createClass({
   getInitialState: function() {
     return {
-      match: this.props.match,
       display_form: false,
       score_params: {}
     };
@@ -13,12 +12,15 @@ var Match = React.createClass({
     }));
   },
 
+  onMatchUpdate: function(data) {
+    this.props.matchUpdate(data);
+  },
 
   handleUserInput: function(name, points) {
     var score = this.state.score_params;
     score[name] = points;
     this.setState({score_params: score});
-    this.state.match.score_params = this.state.score_params;
+    this.props.match.score_params = this.state.score_params;
   },
 
   handleFormSubmit: function() {
@@ -26,22 +28,20 @@ var Match = React.createClass({
       type: 'PATCH',
       url: '/matches/' + this.props.match.id,
       dataType: 'json',
-      data: {score_params: this.state.match.score_params}
+      data: {score_params: this.props.match.score_params}
     }).done(function(data) {
-        this.setState({match: data});
-
+        this.props.onScoreSubmit(data);
       }.bind(this));
     this.setState({display_form: false});
-    console.log(this.props.match);
   },
 
   render: function(){
     var display_form = this.state.display_form
     var match_display;
 
-    if (this.state.match.status === 'Played') {
+    if (this.props.match.status === 'Played') {
       match_display = (
-        <FinalScore match={this.state.match}
+        <FinalScore match={this.props.match}
         key={'finalscore' + this.props.match.id}/>
       )
     } else {
--- app/assets/javascripts/components/player_fixtures.js.jsx
+++ app/assets/javascripts/components/player_fixtures.js.jsx
@@ -1,8 +1,12 @@
 var PlayerFixtures = React.createClass({
   getInitialState: function(){
-    return{
-      player: this.props.player
+    return {
+      match: this.props.match
     };
+  },
+
+  handleScoreSubmit: function(data){
+    this.setState({match: data});
   },
 
   render: function() {
@@ -11,9 +15,9 @@ var PlayerFixtures = React.createClass({
 
     {this.props.fixtures.map(function(match, j){
       if (match.player_one.id === player.id || match.player_two.id === player.id) {
-          player_matches.push(<Match match={match} key={j} score_params={match.score_params} />);
+          player_matches.push(<Match match={this.state.match} key={j} score_params={match.score_params} onScoreSubmit={this.handleScoreSubmit} />);
       }
-    })}
+    }.bind(this))}
 
     return (
       <div className="fixtures hidden" id={player.first_name}>
