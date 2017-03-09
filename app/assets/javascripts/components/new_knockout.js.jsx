13242d12be2ef3224a9a9a1891f48d8761064dc8
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-03-09 15:52:59 +0000

    adding users to competition part of form in progress

--- app/assets/javascripts/components/new_knockout.js.jsx
+++ app/assets/javascripts/components/new_knockout.js.jsx
@@ -1,7 +1,16 @@
 var NewKnockout = React.createClass({
   render: function() {
     return (
-        <p>Step Two Knockout Bitches!</p>
+      <div>
+        <label for="number-of-players">Choose # of players:</label>
+        <select name={this.props.competition.number_of_players} id="number-of-players">
+          <option value="2">2</option>
+          <option value="4">4</option>
+          <option value="8">8</option>
+          <option value="16">16</option>
+        </select>
+
+      </div>
     );
   }
 })
