0ed74ae9b78345c2d543ea8abffe365340895c96
Romain Campech <romain.campech@city.ac.uk>
2017-03-13 17:06:31 +0000

    ActionCable setup working for direct messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
 
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+
   validates_presence_of :body, :conversation_id, :user_id
 end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
+require 'action_cable/engine'
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
