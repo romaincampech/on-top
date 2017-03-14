54eb165a38f34cf3477a9867aca8cc7f80749ebd
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-03-14 19:19:47 +0000

    react league table displaying points

--- app/assets/javascripts/components/league/league_table_row.js.jsx
+++ app/assets/javascripts/components/league/league_table_row.js.jsx
@@ -1,16 +1,17 @@
 var LeagueTableRow = React.createClass({
   render: function() {
+    console.log(this.props.player)
     return (
-    // <tr>
-    //         <td>x</td>
-    //         <td><span>{this.props.player.user.first_name %></span></div></td>
-    //         <td>{this.props.player.user.played_matches.where(competition_id: @competition.id).count %></td>
-    //         <td>{this.props.player.user.played_matches.where(competition_id: @competition.id, winner_id: player.user.id).count %></td>
-    //         <td>{this.props.player.user.played_matches.where(competition_id: @competition.id, winner_id: 0).count %></td>
-    //         <td>{this.props.player.user.played_matches.where(competition_id: @competition.id).count - (player.user.played_matches.where(competition_id: @competition.id, winner_id: player.user.id).count + player.user.played_matches.where(competition_id: @competition.id, winner_id: 0).count) %></td>
-    //         <td><strong><%= player.points %></strong></td>
-    //       </tr>
-      <div>Shit</div>
+    <tr>
+      <td>x</td>
+      <td><span>{this.props.player.user_id}</span></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>{this.props.player.points}</td>
+    </tr>
+
     );
   }
 })
