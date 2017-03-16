621808c624dc770bb63942d2fead3ddbbc4dc7f4
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-03-16 18:39:18 +0000

    React knockout fucking with me

--- app/assets/javascripts/components/knockout_competition.js.jsx
+++ app/assets/javascripts/components/knockout_competition.js.jsx
@@ -1,15 +1,17 @@
 var KnockoutCompetition = React.createClass({
 
 
+
   render: function() {
     console.log(this.props.matches[0].competition.number_of_rounds);
 
-    var rounds_display = [];
+var rounds_display = [];
     var rounds = this.props.matches[0].competition.number_of_rounds;
 
     for (var i=1; i=rounds; i++) {
       rounds_display.push(<KnockoutRound matches={this.props.matches} round={i} key={i} />);
     }
+
 
     return (
       <div>
--- app/assets/javascripts/components/knockout_round.js.jsx
+++ app/assets/javascripts/components/knockout_round.js.jsx
@@ -1,9 +1,27 @@
 var KnockoutRound = React.createClass({
+
+  updateMatch: function() {
+    console.log('shit');
+  },
+
   render: function() {
     console.log(this.props.matches[0]);
     console.log(this.props.round);
+
+    var round = this.props.round;
+    var round_matches = [];
+
+    {this.props.matches.map(function(match, j) {
+
+        round_matches.push(<Match match={match} key={j} score_params={match.score_params} setMatch={this.updateMatch}/>);
+      }
+    )}
+
     return (
+      <div>
       <h1> Fuck </h1>
+        {round_matches}
+      </div>
     )
   }
 })
